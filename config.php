482b918f3926a3959e7666e768841f355b8d9f5d
Ehsan Heydary <ehsanheydarye@gmail.com>
2021-07-19 03:40:47 -0700

    clean code

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 switch (defined('flag')):
     case false :
         die('<p style="font-size: 28px;"><b>Access forbidden!</b></p>
-You don\'t have permission to access the requested directory. There is either no index document or the directory is read-protected.
-If you think this is a server error, please contact the webmaster.
+        You don\'t have permission to access the requested directory. There is either no index document or the directory is read-protected.
+        If you think this is a server error, please contact the webmaster.
 <p style="font-size: 28px">Error 403 <p>');
         break;
 endswitch;
